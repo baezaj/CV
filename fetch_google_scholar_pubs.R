c77e03df6ddd18cf9e12b0378d0fe41f14d60a3d
baezaj <baeza@pennmedicine.upenn.edu>
2020-11-08 15:23:02 -0500

    initial commit

---
+++ fetch_google_scholar_pubs.R
@@ -0,0 +1,21 @@
+
+library(vitae)
+library(tidyverse)
+library(rio)
+library(lubridate)
+library(rorcid)
+library(glue)
+library(scholar)
+
+
+# Retreiving citations from google scholar
+pubs <- get_publications("lIYgwaYAAAAJ")
+
+# Getting complete author list
+pubs$author <- unlist(lapply(pubs$pubid, function(x){
+  get_complete_authors("lIYgwaYAAAAJ", x)
+}))
+
+
+export(pubs, file = "data/publications_google_scholar.csv")
+
